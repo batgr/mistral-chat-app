ac13353d414ddf38ebc29f3c9a3f13f0cee3df31
batgr <80541212+batgr@users.noreply.github.com>
2025-01-25 17:38:14 +0100

    a personalized input field

---
+++ app/Components/Input.jsx
@@ -0,0 +1,10 @@
+
+const Input = ()=>{
+    return(
+        <>
+            <input type="text" className="input"/>
+        </>
+    )
+}
+
+export default Input;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
+import Input from "@/app/Components/Input";
+import "./page.css";
+
 
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <main className="main">
+      <Input/>
+    </main>
   );
 }
